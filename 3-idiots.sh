d55b8111d22a81b365b89fbf5b424ff5855402e6
Ubuntu <ubuntu@ip-172-31-14-97.ap-south-1.compute.internal>
2025-02-13 09:50:53 +0000

    first commit

---
+++ 3-idiots.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+
+# user define variable
+#name="shiv"
+#age="23"
+
+#echo "your name is $name"
+#echo "your age is $age"
+
+# shell / environment variable bhi hote hai (pre-defined variable)
+
+#echo "current logged in user is : $USER"
+
+
+
+#read -p "what is your name please enter here :" fullname
+
+#echo "your name is $fullname "
+
+#arguments(arg1 arg2)
+
+#./3idiots.sh shiv rohan
+
+echo "first name is : $1"
+
+echo "second name is : $2"
+
+echo "third name is : $3"
+
+echo "total number of names : $#"
+
+echo "here all name are $@"
+echo "hey last end " 
+
+
